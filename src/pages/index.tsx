0381f279d7af1f6595fe7bdbcc83b498acc84179
Alyson Vilela do Nascimento <alysonvn@MMB026004CPS.local>
2023-04-18 23:16:05 -0300

    feat: add linters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { Locale, usePersonalsQuery } from '@/graphql/generated'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-const {data} = usePersonalsQuery({
- variables: {
-  locales: [Locale.PtBr, Locale.En]
- }
-})
-
+  const { data } = usePersonalsQuery({
+    variables: {
+      locales: [Locale.PtBr, Locale.En],
+    },
+  })
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main
+      className={`${inter.className}flex min-h-screen flex-col items-center justify-between p-24`}
+    >
       {data?.personals[0].description}
     </main>
   )
